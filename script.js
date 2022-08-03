8fb9a25c419167beb80499fa52efa766def63434
ditod1 <orlandodiazconde@gmail.com>
2022-08-03 11:17:10 -0500

    Add play a round function

--- script.js
+++ script.js
@@ -13,4 +13,39 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if (player === computerSelection) return "It's A Tie!";
+    else if (player === "Rock") {
+        if (computerSelection === "Scissors") {
+            return "You Win! Rock beats Scissors"
+        }
+        else {
+            return "You Lose! Paper beats Rock"
+        }
+    }
+    else if (player === "Paper") {
+        if (computerSelection === "Rock") {
+            return "You Win! Paper beats Rock"
+        }
+        else {
+            return "You Lose! Scissors beats Paper"
+        }
+    }
+    else if (player == "Scissors") {
+        if (computerSelection === "Paper") {
+            return "You Win! Scissors beats Paper"
+        }
+        else {
+            return "You Lose! Rock beats Scissors"
+        }
+    }
+    else {
+        return "Please Enter Either Rock, Paper or Scissors"
+    }
+}
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
