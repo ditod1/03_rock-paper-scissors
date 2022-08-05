5f9fadd170c9c439ab49b1a8296932ca59575029
ditod1 <orlandodiazconde@gmail.com>
2022-08-04 20:03:47 -0500

    Change input logic to UI buttons

--- script.js
+++ script.js
@@ -1,71 +1,69 @@
 //Rock, paper, scissors game
 
 function getComputerChoice() {
-    let randNumber = Math.floor(Math.random() * 3);
-    if (randNumber < 1) {
-        return "Rock"
-    }
-    else if (randNumber < 2) {
-        return "Paper"
-    }
-    else {
-        return "Scissors"
-    }
+  let randNumber = Math.floor(Math.random() * 3);
+  if (randNumber < 1) {
+    return "Rock"
+  }
+  else if (randNumber < 2) {
+    return "Paper"
+  }
+  else {
+    return "Scissors"
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) return "Tie";
-    else if (playerSelection === "Rock") {
-        if (computerSelection === "Scissors") {
-            return "Win"
-        }
-        else {
-            return "Lose"
-        }
-    }
-    else if (playerSelection === "Paper") {
-        if (computerSelection === "Rock") {
-            return "Win"
-        }
-        else {
-            return "Lose"
-        }
-    }
-    else if (playerSelection == "Scissors") {
-        if (computerSelection === "Paper") {
-            return "Win"
-        }
-        else {
-            return "Lose"
-        }
+  if (playerSelection === computerSelection) return "Tie";
+  else if (playerSelection === "Rock") {
+    if (computerSelection === "Scissors") {
+      return "Win"
     }
     else {
-        return null
+      return "Lose"
     }
+  }
+  else if (playerSelection === "Paper") {
+    if (computerSelection === "Rock") {
+      return "Win"
+    }
+    else {
+      return "Lose"
+    }
+  }
+  else if (playerSelection == "Scissors") {
+    if (computerSelection === "Paper") {
+      return "Win"
+    }
+    else {
+      return "Lose"
+    }
+  }
+  else {
+    return null
+  }
 }
 
-function game() {
-    playerScore = 0;
-    computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const input = prompt("Enter your choice (Either rock, paper or scissors):");
-        const playerSelection = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
-        const computerSelection = getComputerChoice();
-        const result = playRound(playerSelection, computerSelection);
-        if (result == "Win") {
-            playerScore++;
-            console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
-        }
-        else if (result == "Lose") {
-            computerScore++;
-            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-        }
-        else if (result == "Tie") {
-            console.log("It's a tie");
-        }
-    }
-    console.log(`The final score is: Player ${playerScore} - Computer ${computerScore}`)
+function game(playerSelection) {
+  const computerSelection = getComputerChoice();
+  const result = playRound(playerSelection, computerSelection);
+  if (result == "Win") {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
+  }
+  else if (result == "Lose") {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+  }
+  else if (result == "Tie") {
+    console.log("It's a tie");
+  }
 }
 
+btn = document.querySelectorAll('button');
 
-game();
+function choice(e) {
+  game(this.id);
+}
+
+btn.forEach((button) => {
+  button.addEventListener('click', choice);
+});
